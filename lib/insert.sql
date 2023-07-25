3183ce4ac9e5827acba157328a6e1b5d26037719
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-25 17:04:59 -0600

    finishes lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'black', 'chill', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'orange', 'nice', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 66, 'M', 'yellow', 'cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'green', 'silly', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 8, 'M', 'blue', 'funny', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 23, 'M', 'neon pink', 'awesome', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'M', 'ocean green', 'alchoholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 89, 'M', 'cyan', 'chill', 0);
